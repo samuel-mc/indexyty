d3f6053e1105c5f942ca0f0345fae84a248b23e1
Samuel Medina <smdoomed@gmail.com>
2022-03-19 10:09:01 -0600

    cambios solicitados

---
+++ index.js
@@ -0,0 +1,11 @@
+const header = document.querySelector(".header");
+window.addEventListener("scroll", function(e) {
+    if (window.scrollY >  80) {
+        header.classList = "header bg";
+    } else {
+        header.classList = "header";
+    }
+    console.log(window.scrollY)
+});
+
+// console.log(window.scrollY);
